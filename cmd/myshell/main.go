a8d19f3db002f98e7afbc7b57bd64e15ced2295f
Robin Chowdhury <robinch88@gmail.com>
2024-06-26 15:53:42 +0200

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,16 +8,18 @@ import (
 )
 
 func main() {
-	fmt.Print("$ ")
+	for {
+		fmt.Print("$ ")
 
-	br := bufio.NewReader(os.Stdin)
-	input, err := br.ReadString('\n')
+		br := bufio.NewReader(os.Stdin)
+		input, err := br.ReadString('\n')
 
-	input = strings.TrimSpace(input)
+		input = strings.TrimSpace(input)
 
-	if err != nil {
-		panic(fmt.Sprintf("Could not read input, error: %v", err))
+		if err != nil {
+			panic(fmt.Sprintf("Could not read input, error: %v", err))
+		}
+
+		fmt.Printf("%s: command not found\n", input)
 	}
-
-	fmt.Printf("%s: command not found\n", input)
 }
